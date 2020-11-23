63c866accd9966d35aa106ac15601609fdda13b4
Wiz <y676611165@163.com>
2020-11-23 20:19:51 +0800

    修改: 调整输出目录

--- html2png.py
+++ html2png.py
@@ -32,7 +32,7 @@ def html2png_display(filename, png_path):
     """
 
     # Save the map as an HTML file
-    delay = 1
+    # delay = 1
     print(filename)
     fn = filename
     output_dir = png_path
@@ -45,14 +45,14 @@ def html2png_display(filename, png_path):
     # ..that displays the map...
     browser.get(tmpurl)
     # Give the map tiles some time to load
-    time.sleep(delay)
+    # time.sleep(delay)
     # Grab the screenshot
-    dir_exists(output_dir)
+    # dir_exists(output_dir)
     fn = fn.split("/")[-1]
     output_name = output_dir + fn.split(".")[0]+'.png'
     elem=browser.find_element_by_id("Main")
-    elem.screenshot(output_name)
-    os.chmod(output_name, 777)
+    elem.screenshot(output_dir)
+    os.chmod(output_dir, 777)
     # Close the browser
     browser.quit()
     return output_name
