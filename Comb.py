42ccc3dba635692d87665ccc512a94d991824e7b
Wiz <y676611165@163.com>
2020-11-27 20:58:57 +0800

    修改: 存储目录，同时保存HTML

--- Comb.py
+++ Comb.py
@@ -4,31 +4,40 @@ import os
 import csv
 from shutil import copytree, rmtree
 from PIL import Image
+""" 
+TouHou 卡牌生成器，利用HTML模板截图生成
+"""
 # 生成器html模板所在位置
 temple = "html/Sample.html"
+if not os.path.exists('./card/'):
+	os.mkdir("./card")
 # 需要生成的目录
-game=('TH6',)
+game=('TH10','TH11')
 with open(temple,encoding='utf-8') as f:
 	html=f.read()
 for num in game:
 	#设置目录
 	gamedir="./img/"+num+'/'
 	titledir=gamedir+'title.csv'#称号和名字
-	shadir='../2/'#立绘填充
-	maindir='../1/'#立绘
-	htmldir = gamedir+'html/Sample.html'
-	carddir = gamedir+"card/"
+	shadir='../../2/'#立绘填充
+	maindir='../../1/'#立绘
+	htmlfolder = gamedir+'html/'#存放html目录
+	carddir = "./card/"+num+"/"#存放卡的目录
 	#创建工作html文件夹
-	if os.path.exists(gamedir+'html/'):
-		rmtree(gamedir+'html/')
-	copytree('.html/',gamedir+'html/')
-	os.mkdir(gamedir+"card/")#卡片保存目录
+	if os.path.exists(htmlfolder):
+		#如果存在就刷新所有目录
+		rmtree(htmlfolder)
+		os.mkdir(htmlfolder)
+	if not os.path.exists(carddir):
+		os.mkdir(carddir)  # 卡片保存目录
 	with open(titledir,encoding='utf-8') as f:
 		title=csv.reader(f)
 		for i,row in enumerate(title):
 			#批量替换素材
-			shadirc=shadir+str(i+1)+'.png'
-			maindirc=maindir+str(i+1)+'.png'
+			htmldir = htmlfolder+str(i+1)+'/Sample.html'
+			copytree('./html/', htmlfolder+str(i+1))
+			shadirc=shadir+str(i+1)+'.png'#当前卡图
+			maindirc=maindir+str(i+1)+'.png'#当前卡图
 			curcard = re.sub("mytitle",row[0],html)
 			curcard = re.sub("myname",row[1],curcard)
 			curcard = re.sub("TH6",num,curcard)
@@ -36,14 +45,14 @@ for num in game:
 			curcard = re.sub("Sample_main.png", maindirc, curcard)
 			with open(htmldir,'w',encoding='utf-8') as f:
 				f.write(curcard)
-			html2png_display(htmldir, carddir+'temp.png')
+			html2png_display(htmldir, htmlfolder+'temp.png')
 			#设置色彩模式和dpi
-			im = Image.open(carddir+'temp.png')
+			im = Image.open(htmlfolder+'temp.png')
 			im = im.convert(mode="CMYK")
-			im.save(carddir+row[1]+'.jpg', dpi=(300.0, 300.0))  # 保存文件
-			print(row[1],'OK')
-		os.remove(carddir+'temp.png')
-	rmtree(gamedir+'html/')#删除html工作目录
+			im.save(carddir+str(i+1)+'.jpg', dpi=(300.0, 300.0))  # 保存文件
+			os.remove(htmlfolder+'temp.png')
+			print(row[1], 'OK')
+	# rmtree(htmlfolder)#删除html工作目录
 
 		 
 			
