619901860dfb7d6a7ba4c02f6360545c979484d1
Wiz <y676611165@163.com>
2020-11-30 16:26:13 +0800

    修改: 代码重构 拆分功能

--- Comb.py
+++ Comb.py
@@ -7,12 +7,61 @@ from PIL import Image
 """ 
 TouHou 卡牌生成器，利用HTML模板截图生成
 """
+game = ('TH',)
+def ChangeDpi(path,out_path):
+	"""
+	设置色彩模式，修改生成PNG DPI 并转换成jpg
+	"""
+	im = Image.open(path)
+	im = im.convert(mode="CMYK")
+	im.save(out_path, dpi=(300.0, 300.0))  # 保存文件
+	os.remove(path)
+
+def SubHTML(html,row,num,main,sha,**kw):
+	"""
+	替换HTML文件
+	"""
+	curcard = re.sub("mytitle", row[0], html)
+	curcard = re.sub("myname",row[1],curcard)
+	curcard = re.sub("TH6",num,curcard)
+	curcard = re.sub("Sample_sha.png", sha, curcard)
+	curcard = re.sub("Sample_main.png", main, curcard)
+	return curcard
+
+class Path_check(object):
+	"""
+	检查目录是否存在
+	"""
+	def __init__(self):
+		pass
+
+	def card(self):
+		"""
+		card path
+		"""
+		if not os.path.exists('./card/'):
+			os.mkdir("./card")
+	def html(self,path):
+		"""
+		gamedir 下 html工作目录
+		"""
+		if os.path.exists(path):
+			#如果存在就刷新所有目录
+			rmtree(path)
+			os.mkdir(path)
+	def cardsub(self,path):
+		"""
+		card 下 gamedir
+		"""
+		if not os.path.exists(path):
+			os.mkdir(path)  # 卡片保存目录
+	
 # 生成器html模板所在位置
-temple = "html/Sample.html"
-if not os.path.exists('./card/'):
-	os.mkdir("./card")
+
+Pk=Path_check()
+Pk.card()#生成卡牌的目录
 # 需要生成的目录
-game=('TH10','TH11')
+temple = "html/Sample.html"
 with open(temple,encoding='utf-8') as f:
 	html=f.read()
 for num in game:
@@ -24,12 +73,8 @@ for num in game:
 	htmlfolder = gamedir+'html/'#存放html目录
 	carddir = "./card/"+num+"/"#存放卡的目录
 	#创建工作html文件夹
-	if os.path.exists(htmlfolder):
-		#如果存在就刷新所有目录
-		rmtree(htmlfolder)
-		os.mkdir(htmlfolder)
-	if not os.path.exists(carddir):
-		os.mkdir(carddir)  # 卡片保存目录
+	Pk.html(htmlfolder)
+	Pk.cardsub(carddir)
 	with open(titledir,encoding='utf-8') as f:
 		title=csv.reader(f)
 		for i,row in enumerate(title):
@@ -38,21 +83,12 @@ for num in game:
 			copytree('./html/', htmlfolder+str(i+1))
 			shadirc=shadir+str(i+1)+'.png'#当前卡图
 			maindirc=maindir+str(i+1)+'.png'#当前卡图
-			curcard = re.sub("mytitle",row[0],html)
-			curcard = re.sub("myname",row[1],curcard)
-			curcard = re.sub("TH6",num,curcard)
-			curcard = re.sub("Sample_sha.png", shadirc, curcard)
-			curcard = re.sub("Sample_main.png", maindirc, curcard)
+			curcard=SubHTML(html,row=row,num=num,sha=shadirc,main=maindirc)
 			with open(htmldir,'w',encoding='utf-8') as f:
 				f.write(curcard)
 			html2png_display(htmldir, htmlfolder+'temp.png')
-			#设置色彩模式和dpi
-			im = Image.open(htmlfolder+'temp.png')
-			im = im.convert(mode="CMYK")
-			im.save(carddir+str(i+1)+'.jpg', dpi=(300.0, 300.0))  # 保存文件
-			os.remove(htmlfolder+'temp.png')
+			ChangeDpi(htmlfolder+'temp.png', carddir+str(i+1)+'.jpg')
 			print(row[1], 'OK')
-	# rmtree(htmlfolder)#删除html工作目录
 
 		 
 			
